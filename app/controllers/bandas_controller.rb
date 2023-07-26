3a3beeacdcdc4fa21933ccdd5b94ee8abd6d9c26
MarcelFeo <marcelfeo29@gmail.com>
2023-07-26 11:28:05 -0300

    feat: add CRUD

--- app/controllers/bandas_controller.rb
+++ app/controllers/bandas_controller.rb
@@ -1,2 +1,45 @@
 class BandasController < ApplicationController
+  before_action :set_banda, only: [:show, :update, :destroy]
+
+  def index
+    @bandas = Banda.all
+    render json: @bandas
+  end
+
+  def show
+    render json: @banda
+  end
+
+  def create
+    @banda = Banda.new(banda_params)
+
+    if @banda.save
+      render json: @banda, status: :created
+    else
+      render json: @banda.errors, status: :bad_request
+    end
+  end
+
+  def update
+    if @banda.update(banda_params)
+      render json: @banda
+    else
+      render json: @banda.errors, status: :bad_request
+    end
+  end
+
+  def destroy
+    @banda.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_banda
+    @banda = Banda.find(params[:id])
+  end
+
+  def banda_params
+    params.require(:banda).permit(:name)
+  end
 end
